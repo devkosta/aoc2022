51246b31db5da6d9a54376a49b7bbef653c46449
Kosta Hassouros <kostahassouros@Kostas-MacBook-Pro.local>
2022-12-04 16:09:13 +1000

    Solution to D03: Solved Part 02

--- src/main/java/devkosta/aoc/D03/Main.java
+++ src/main/java/devkosta/aoc/D03/Main.java
@@ -16,22 +16,22 @@ public class Main {
         P02.solve(Main::solvePartTwo);
     }
 
-    public static int commonPriority(List<String> strings) {
-        // Common character in list of strings
-        var c = strings.stream().reduce((s1, s2) -> commonChars(s1, s2)).get().charAt(0);
+    public static int commonPriority(List<String> rucksacks) {
+        // Common character in rucksacks
+        char c = rucksacks.stream().reduce(Main::commonChars).get().charAt(0);
 
-        // Convert to priority
         return c - 'a' + (isUpperCase(c) ? 58 : 0) + 1;
     }
 
     static String commonChars(String a, String b) {
-        return a.chars().filter(i1 -> b.chars().anyMatch(i2 -> i1 == i2)).distinct().mapToObj(c -> Character.toString(c)).collect(Collectors.joining());
+        return a.chars().filter(i1 -> b.chars().anyMatch(i2 -> i1 == i2)).distinct().mapToObj(Character::toString).collect(Collectors.joining());
     }
 
     public static int solvePartOne(String[] input) {
         int totalSum = 0;
         for (String rucksack : input) {
             int n = rucksack.length(), mid = n / 2;
+
             String rucksackOne = rucksack.substring(0, mid);
             String rucksackTwo = rucksack.substring(mid, n);
 
@@ -43,8 +43,14 @@ public class Main {
 
     public static int solvePartTwo(String[] input) {
         int totalSum = 0;
-        for (String rucksack : input) {
-            // Implementation here
+        for (int i = 0; i < input.length; i += 3) {
+            String rucksackOne = input[i];
+            String rucksackTwo = input[i + 1];
+            String rucksackThr = input[i + 2];
+
+            String newRucksack = commonChars(rucksackOne, rucksackTwo);
+
+            totalSum += commonPriority(List.of(newRucksack, rucksackThr));
         }
 
         return totalSum;
